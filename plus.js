17102de3e4ef5370e4b1a6fe1cf57e3e4d79e5fb
Chung NT <chungnt88@gmail.com>
2022-11-28 16:12:50 +0700

    fix

--- plus.js
+++ plus.js
@@ -3,12 +3,19 @@ const random_int = (min, max) => Math.floor(Math.random() * (max - min + 1) + mi
 angular.module("app", []).controller("controller", function ($scope) {
     let min = Number(con7.params.min);
     let max = Number(con7.params.max);
+    let plus = con7.params.minus ? false : true;
 
     let ok = [];
-    for (let i = 0; i < 100; i++) {
-        ok.push(con7.params.plus ? random_plus(min, max) : random_minus(min, max));
+    let temp = [];
+    for (let i = 0; i < 172; i++) {
+        temp.push(plus ? random_plus(min, max) : random_minus(min, max));
+        if (i % 4 == 3) {
+            ok.push(temp);
+            temp = [];
+        }
     }
     $scope.model = ok;
+    $scope.title = plus ? "Phép cộng" : "Phép trừ";
 });
 
 function random_plus(min, max) {
